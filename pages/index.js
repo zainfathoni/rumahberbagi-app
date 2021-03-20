5584f3c5987d5d7194111f5bed034cd7f92ebd81
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-20 20:40:24 +0800

    Replace Inter font with Nunito font

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,11 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
-        <link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
 
       <main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
       },
     },
   },
