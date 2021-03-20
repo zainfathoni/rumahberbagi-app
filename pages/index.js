81c138d09c7a5a061362a8c51d0ff4ff8f170d3c
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-20 22:19:28 +0800

    Implement DNS Prefetching to optimize Google Fonts performance
    https://www.smashingmagazine.com/2019/06/optimizing-google-fonts-performance/

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Kelas Rumah Berbagi</title>
         <link rel='icon' href='/favicon.ico' />
+        <link rel='dns-prefetch' href='//fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap'
