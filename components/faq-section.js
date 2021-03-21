1a78ca9ff6de09700407fdc2bfd7c73dda750779
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-21 09:02:41 +0800

    Update FaqSection to use Compound Components pattern
    https://epicreact.dev/modules/advanced-react-patterns/compound-components-extra-credit-solution-1

--- components/faq-section.js
+++ components/faq-section.js
@@ -1,21 +1,36 @@
 import * as React from 'react'
 
+export const FaqDescription = ({ children }) => <p className='mt-4 text-lg text-gray-500'>{children}</p>
+
+export const FaqContent = ({ children }) => <dl className='space-y-12'>{children}</dl>
+
 export const FaqQuestion = ({ children }) => <dt className='text-lg leading-6 font-medium text-gray-900'>{children}</dt>
 
 export const FaqAnswer = ({ children }) => <dd className='mt-2 text-base text-gray-500'>{children}</dd>
 
-export const FaqSection = ({ title, children, description }) => (
-  <div className='bg-white' id='faq'>
-    <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8'>
-      <div className='lg:grid lg:grid-cols-3 lg:gap-8'>
-        <div>
-          <h2 className='text-3xl font-extrabold text-gray-900'>{title}</h2>
-          <p className='mt-4 text-lg text-gray-500'>{description}</p>
-        </div>
-        <div className='mt-12 lg:mt-0 lg:col-span-2'>
-          <dl className='space-y-12'>{children}</dl>
+export const FaqSection = ({ title, children }) => {
+  let description, qa
+  React.Children.forEach(children, (child) => {
+    switch (child.type) {
+      case FaqDescription:
+        description = child
+        break
+      case FaqContent:
+        qa = child
+        break
+    }
+  })
+  return (
+    <div className='bg-white' id='faq'>
+      <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8'>
+        <div className='lg:grid lg:grid-cols-3 lg:gap-8'>
+          <div>
+            <h2 className='text-3xl font-extrabold text-gray-900'>{title}</h2>
+            {description}
+          </div>
+          <div className='mt-12 lg:mt-0 lg:col-span-2'>{qa}</div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import {
   BenefitTopContainer,
 } from '../components/benefit-section'
 import { CtaButton, CtaSection, CtaDescription, CtaTitle } from '../components/cta-section'
-import { FaqAnswer, FaqQuestion, FaqSection } from '../components/faq-section'
+import { FaqAnswer, FaqDescription, FaqContent, FaqQuestion, FaqSection } from '../components/faq-section'
 import { HeroSection } from '../components/hero-section'
 import { FileInvoiceIcon } from '../components/icons/file-invoice'
 import { FileSignatureIcon } from '../components/icons/file-signature'
@@ -75,57 +75,60 @@ export default function Home() {
             </BenefitItem>
           </BenefitBottomContainer>
         </BenefitSection>
-        <FaqSection
-          description={
-            <>
-              Tidak menemukan jawaban yang Anda cari? Hubungi kami{' '}
-              <a href='#' className='font-medium text-indigo-600 hover:text-indigo-500'>
-                di sini
-              </a>
-              .
-            </>
-          }
-          title='FAQ (Tanya Jawab)'
-        >
-          <div>
-            <FaqQuestion>Kelas ini untuk siapa?</FaqQuestion>
-            <FaqAnswer>
-              Kelas ini untuk para orang tua, khususnya untuk anak di bawah usia sekolah, tetapi tidak menutup
-              kemungkinan orang tua dengan anak di usia sekolah masih membutuhkannya karena bisa jadi ada aspek yang
-              terlewat dari fondasi yang harus dipersiapkan di usia prasekolah.
-            </FaqAnswer>
-          </div>
-          <div>
-            <FaqQuestion>Berapa lama kelas ini akan berlangsung?</FaqQuestion>
-            <FaqAnswer>
-              Setidaknya akan ada dua pertemuan yang terdiri dari pembahasan materi dan praktik pembuatan rencana
-              pendidikan untuk anak prasekolah. Tidak menutup kemungkinan jumlah pertemuan akan ditambah sesuai dengan
-              kebutuhan.
-            </FaqAnswer>
-          </div>
-          <div>
-            <FaqQuestion>Berapa biaya kelas ini? Kapan saya harus membayarnya?</FaqQuestion>
-            <FaqAnswer>
-              Biaya kelas ini Rp 100.000,- yang dapat dibayarkan setelah Anda mendapatkan konfirmasi dari kami bahwa
-              Anda mendapatkan slot untuk bergabung di kelas ini.
-            </FaqAnswer>
-          </div>
-          <div>
-            <FaqQuestion>Mengapa jumlah peserta hanya dibatasi sebanyak 20 orang?</FaqQuestion>
-            <FaqAnswer>
-              Supaya proses belajar bisa lebih fokus dan aktivitas tanya jawab bisa dilakukan secara lebih intensif.
-            </FaqAnswer>
-          </div>
-          <div>
-            <FaqQuestion>
-              Saya gagal mendapatkan slot di kelas ini. Lantas bagaimana saya bisa mengikuti kelas ini?
-            </FaqQuestion>
-            <FaqAnswer>
-              Pertama-tama, pastikan dulu bahwa Anda telah terdaftar ke dalam daftar antrian calon peserta. Apabila
-              kelas untuk angkatan berikutnya telah kami buka, kami akan kirimkan email kepada Anda berdasarkan urutan
-              antrian Anda.
-            </FaqAnswer>
-          </div>
+        <FaqSection title='FAQ (Tanya Jawab)'>
+          <FaqDescription>
+            Tidak menemukan jawaban yang Anda cari? Hubungi saya{' '}
+            <a
+              href='https://www.instagram.com/vika.riandini/'
+              className='font-medium text-indigo-600 hover:text-indigo-500'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              melalui Instagram
+            </a>
+            .
+          </FaqDescription>
+          <FaqContent>
+            <div>
+              <FaqQuestion>Kelas ini untuk siapa?</FaqQuestion>
+              <FaqAnswer>
+                Kelas ini untuk para orang tua, khususnya untuk anak di bawah usia sekolah, tetapi tidak menutup
+                kemungkinan orang tua dengan anak di usia sekolah masih membutuhkannya karena bisa jadi ada aspek yang
+                terlewat dari fondasi yang harus dipersiapkan di usia prasekolah.
+              </FaqAnswer>
+            </div>
+            <div>
+              <FaqQuestion>Berapa lama kelas ini akan berlangsung?</FaqQuestion>
+              <FaqAnswer>
+                Setidaknya akan ada dua pertemuan yang terdiri dari pembahasan materi dan praktik pembuatan rencana
+                pendidikan untuk anak prasekolah. Tidak menutup kemungkinan jumlah pertemuan akan ditambah sesuai dengan
+                kebutuhan.
+              </FaqAnswer>
+            </div>
+            <div>
+              <FaqQuestion>Berapa biaya kelas ini? Kapan saya harus membayarnya?</FaqQuestion>
+              <FaqAnswer>
+                Biaya kelas ini Rp 100.000,- yang dapat dibayarkan setelah Anda mendapatkan konfirmasi dari kami bahwa
+                Anda mendapatkan slot untuk bergabung di kelas ini.
+              </FaqAnswer>
+            </div>
+            <div>
+              <FaqQuestion>Mengapa jumlah peserta hanya dibatasi sebanyak 20 orang?</FaqQuestion>
+              <FaqAnswer>
+                Supaya proses belajar bisa lebih fokus dan aktivitas tanya jawab bisa dilakukan secara lebih intensif.
+              </FaqAnswer>
+            </div>
+            <div>
+              <FaqQuestion>
+                Saya gagal mendapatkan slot di kelas ini. Lantas bagaimana saya bisa mengikuti kelas ini?
+              </FaqQuestion>
+              <FaqAnswer>
+                Pertama-tama, pastikan dulu bahwa Anda telah terdaftar ke dalam daftar antrian calon peserta. Apabila
+                kelas untuk angkatan berikutnya telah kami buka, kami akan kirimkan email kepada Anda berdasarkan urutan
+                antrian Anda.
+              </FaqAnswer>
+            </div>
+          </FaqContent>
         </FaqSection>
         <CtaSection>
           <CtaTitle>Tahun Prasekolahku</CtaTitle>
