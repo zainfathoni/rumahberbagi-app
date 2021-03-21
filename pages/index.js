8f15abdeab4f2bb67f6b2a59b0511c6b91feb3b3
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-21 10:28:53 +0800

    Update favicon to Rumah Berbagi logo

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Kelas Rumah Berbagi</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/rumah-berbagi.jpeg' />
         <link rel='dns-prefetch' href='//fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap' rel='stylesheet' />
