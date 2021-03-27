23ef780a64a32c8bed0d02c64100b8bdb4fbdd27
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-28 00:04:08 +0800

    Add more meta tags in SEO component
    https://metatags.io/

--- components/seo.js
+++ components/seo.js
@@ -1,12 +1,38 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import * as React from 'react'
 
-export const SEO = ({ title }) => (
-  <Head>
-    <title key='title'>{title ? `${title} | Rumah Berbagi` : 'Kelas Rumah Berbagi'}</title>
-    <link rel='icon' href='/rumah-berbagi.jpeg' />
-    <link rel='dns-prefetch' href='//fonts.googleapis.com' />
-    <link rel='preconnect' href='https://fonts.gstatic.com' />
-    <link href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap' rel='stylesheet' />
-  </Head>
-)
+export const SEO = ({ title: _title, description: _description, image: _image }) => {
+  const title = _title ? `${_title} | Rumah Berbagi` : 'Kelas Rumah Berbagi'
+  const description = _description ?? 'Aplikasi Rumah Berbagi'
+  const image = _image ?? '/rumah-berbagi.jpeg'
+
+  const router = useRouter()
+  const url = 'https://app.rumahberbagi.com' + (router.pathname === '/' ? '' : router.pathname)
+
+  return (
+    <Head>
+      <title key='title'>{title}</title>
+
+      <meta key='meta-title' name='title' content={title} />
+      <meta key='meta-description' name='description' content={description} />
+
+      <meta property='og:type' content='website' />
+      <meta key='og-url' property='og:url' content={url} />
+      <meta key='og-title' property='og:title' content={title} />
+      <meta key='og-description' property='og:description' content={description} />
+      <meta key='og-image' property='og:image' content={image} />
+
+      <meta key='twitter-card' property='twitter:card' content='summary_large_image' />
+      <meta key='twitter-url' property='twitter:url' content={url} />
+      <meta key='twitter-title' property='twitter:title' content={title} />
+      <meta key='twitter-description' property='twitter:description' content={description} />
+      <meta key='twitter-image' property='twitter:image' content={image} />
+
+      <link rel='icon' href='/rumah-berbagi.jpeg' />
+      <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap' rel='stylesheet' />
+    </Head>
+  )
+}
--- pages/downloads/ramadhan-preparation.js
+++ pages/downloads/ramadhan-preparation.js
@@ -4,7 +4,11 @@ import { SEO } from '../../components/seo'
 export default function Home() {
   return (
     <div>
-      <SEO title='15 Days Countdown to Ramadhan' />
+      <SEO
+        title='15 Days Countdown to Ramadhan'
+        description='Demi Ramadhan yang lebih bermakna di tahun ini, kami baru saja membuat rencana 15 hari hitung mundur Ramadhan. Download rencananya di sini.'
+        image='/images/15-countdown-to-ramadhan.jpeg'
+      />
 
       <main>
         <ArticleSection />
