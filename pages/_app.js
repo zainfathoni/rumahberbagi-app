ea2d8925f7b577f732c35d157aa3d47f756ed22d
Zain Fathoni <zain.fathoni@gmail.com>
2021-03-28 08:11:48 +0800

    Add Nav component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Nav } from '../components/nav'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-white'>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
