559f9c3949398d2b480cf270faaeeb2522c426b7
Zain Fathoni <zain.fathoni@gmail.com>
2021-04-01 16:06:22 +0800

    Extract ArticleSection props

--- pages/downloads/15-days-countdown-to-ramadhan.js
+++ pages/downloads/15-days-countdown-to-ramadhan.js
@@ -1,4 +1,12 @@
-import { ArticleSection } from '../../components/article-section'
+import {
+  ArticleCategory,
+  ArticleContent,
+  ArticleCTA,
+  ArticleFigure,
+  ArticleSection,
+  ArticleSubtitle,
+  ArticleTitle,
+} from '../../components/article-section'
 import { SEO } from '../../components/seo'
 
 export default function Home() {
@@ -11,7 +19,39 @@ export default function Home() {
       />
 
       <main>
-        <ArticleSection />
+        <ArticleSection>
+          <ArticleCategory>Ramadhan Preparation</ArticleCategory>
+          <ArticleTitle>15 Days Countdown to Ramadhan</ArticleTitle>
+          <ArticleFigure
+            src='/images/15-countdown-to-ramadhan.jpeg'
+            alt='Ramadhan preparation planner'
+            width={2160}
+            height={1620}
+          >
+            15 Days Countdown to Ramadhan
+          </ArticleFigure>
+          <ArticleSubtitle>Sesaat lagi Ramadhan tiba.</ArticleSubtitle>
+          <ArticleContent>
+            <p>
+              Kami ingin Ramadhan tahun ini jadi lebih bermakna, mindful. Bukan sekadar mencapai target banyaknya
+              ibadah, tetapi juga menjalin hubungan yang lebih dekat dengan Allah SWT dan Rasulullah saw.
+            </p>
+            <p>
+              Untuk mencapai itu, kami baru saja membuat rencana 15 hari hitung mundur Ramadhan. Setiap harinya ada
+              waktu khusus yang kami dedikasikan untuk mempersiapkan bulan suci ini.
+            </p>
+            <p>
+              Gimana dengan kalian? Apa yang sudah teman-teman persiapkan? Atau masih bingung bagaimana menyiapkannya?
+            </p>
+            <p>
+              Nah, jika masih bingung, silakan download rencana menyambut ramadhan keluarga kami. Siapa tahu bisa
+              membantu. Kami buat ini jadi dua versi Bahasa Inggris dan Bahasa indonesia.
+            </p>
+          </ArticleContent>
+          <ArticleCTA href='/files/15-days-countdown-to-ramadhan.pdf' download='15 Days Countdown to Ramadhan.pdf'>
+            Download PDF
+          </ArticleCTA>
+        </ArticleSection>
       </main>
     </div>
   )
