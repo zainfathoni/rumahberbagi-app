4dde2db1901645514d988b6ac2f9fcf59d1d7717
Zain Fathoni <zain.fathoni@gmail.com>
2021-04-07 01:34:49 +0800

    Implement screen_view event
    https://medium.com/wesionary-team/integration-of-firebase-analytics-in-nextjs-application-in-minutes-ea5e051ef82a

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { Nav } from '../components/nav'
 import '../styles/globals.css'
 import { analytics } from '../utils/firebase'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
-      analytics()
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url)
+        analytics().logEvent('screen_view')
+      }
+
+      router.events.on('routeChangeComplete', logEvent)
+
+      // For the first page
+      logEvent(window.location.pathname)
+
+      // Remove event listener after unmount
+      return () => {
+        router.events.off('routeChangeComplete', logEvent)
+      }
     }
   }, [])
 
