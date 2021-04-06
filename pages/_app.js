8f914da00a972d48cbd3e7772a32754243c0ae20
Zain Fathoni <zain.fathoni@gmail.com>
2021-04-07 01:10:57 +0800

    Initialize Firebase Analytics conditinally
    https://medium.com/wesionary-team/integration-of-firebase-analytics-in-nextjs-application-in-minutes-ea5e051ef82a

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react'
 import { Nav } from '../components/nav'
 import '../styles/globals.css'
+import { analytics } from '../utils/firebase'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      analytics()
+    }
+  }, [])
+
   return (
     <div className='bg-white'>
       <Nav />
