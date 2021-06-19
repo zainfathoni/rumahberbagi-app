36a6637a5f7b75227b99faae7b05bae267b0bf47
Zain Fathoni <me@zainf.dev>
2021-06-20 07:40:40 +0800

    Add a dummy login API

---
+++ pages/api/login.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  const {
+    body: { email, password },
+    method,
+  } = req;
+  switch (method) {
+    case "POST":
+      if (email && email === password) {
+        res.status(200).json({ token: password });
+      } else {
+        res.status(401).json({ error: "Email/password salah" });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
